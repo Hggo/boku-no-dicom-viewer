3a1294d4fe99e12fd62c7cb6d5ef6e052431653a
Hugo Rodrigues <hugo.ifce@gmail.com>
2018-09-22 20:12:06 -0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../../node_modules/purecss/build/pure-min.css']
 })
 export class AppComponent {
   title = 'boku-no-dicom-viewer';
---
+++ src/app/tela/pesquisa/pesquisa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisa',
+  templateUrl: './pesquisa.component.html',
+  styleUrls: ['./pesquisa.component.css', '../../../../node_modules/purecss/build/pure-min.css']
+})
+export class PesquisaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
