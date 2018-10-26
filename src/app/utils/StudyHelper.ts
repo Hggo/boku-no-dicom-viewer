1c5eedbfccdce7ee5bddb39b035b7c62b93839c1
Z_Usuario Temporario da Pos-graducao <hugo.ifce@gmail.com>
2018-10-26 20:32:38 -0300

    Removes prioritary download of first frame on study helper

--- src/app/utils/StudyHelper.ts
+++ src/app/utils/StudyHelper.ts
@@ -5,33 +5,14 @@ import Instance from '../model/Instance';
 
 export default class StudyHelper {
 
-    private workingThreads: number;
-
     constructor(private study: Study, private studyService: StudyService, private firstTrigger: Function, private finalTrigger: Function) {
-        this.workingThreads = 0;
     }
 
     public prepareStudy() {
         this.studyService.getSeriesFromStudy(this.study).then(series => {
             this.study.series = series;
-            this.resolveFirst();
+            this.resolveEverything();
         });
-    }
-
-    private resolveFirst() {
-        this.studyService.getInstancesFromSerie(this.study.series[0]).then(instances => this.resolveFirstInstanceAndFrame(instances, 0));
-    }
-
-    private resolveFirstInstanceAndFrame(instances: Instance [], serieN: number) {
-        this.study.series[serieN].Instances = instances;
-
-        this.resolveTags(instances[0])
-                .then().then(insttags => {
-                    this.study.series[serieN].Instances[0] = insttags;
-
-                    this.workingThreads++;
-                    this.resolvePixelData(insttags, serieN, 0, 0);
-                });
     }
 
     private resolvePixelData(instance: Instance, serieN: number, instN: number, frameN: number) {
@@ -39,11 +20,8 @@ export default class StudyHelper {
 
             this.study.series[serieN].Instances[instN].frames[frameN].pixelData = instpd.frames[frameN].pixelData;
 
-            this.workingThreads--;
-
             if (frameN === 0 && instN === 0 && serieN === 0) {
                 this.firstTrigger(this.study);
-                this.resolveTheRest();
             } else {
                 this.finalTrigger(instance, serieN, instN, frameN);
             }
@@ -54,7 +32,7 @@ export default class StudyHelper {
         return this.studyService.getTags(instance);
     }
 
-    private resolveTheRest() {
+    private resolveEverything() {
         for (let i = 0; i < this.study.series.length; i++) {
             this.studyService.getInstancesFromSerie(this.study.series[i])
                 .then(instances => this.resolveInstances(instances, i));
@@ -68,7 +46,6 @@ export default class StudyHelper {
         }
 
         for (let j = 0; j < instances.length; j++) {
-            this.workingThreads++;
 
             this.resolveTags(instances[j])
                 .then(insttags => {
@@ -82,7 +59,6 @@ export default class StudyHelper {
                     for (let k = 0; k < insttags.numberOfFrames; k++) {
                         const frame = this.study.series[serieN].Instances[j].frames[k];
                         if (!frame || !frame.pixelData) {
-                            this.workingThreads++;
                             this.resolvePixelData(insttags, serieN, j, k);
                         }
                     }
